8a8997a0aa526623eaafc57cb09d8af9596ad790
Aliceee-glhice <link39709@gmail.com>
2026-04-05 21:12:40 +0700

    Refactor config.js for profile and background updates

--- config.js
+++ config.js
@@ -1,18 +1,60 @@
-// FOTO
-document.getElementById("profile-img").src = "https://imgbox.com/knACbyxd";
+// FOTO PROFIL
+document.getElementById("profile-img").src = "https://images2.imgbox.com/30/f0/CwTGqiYr_o.jpg";
 
 // JUDUL
 document.getElementById("judul").innerText = "Join....❣️";
 
-// BACKGROUND SLIDE (KIRI → KANAN)
-const images = ["https://i.imgur.com/RbpRJsc.jpeg", "https://i.imgur.com/YznES9G.jpeg"];
+// MEMBER ONLINE LIVE
+let current = 7500;
+
+function updateOnline() {
+  let change = Math.floor(Math.random() * 10);
+
+  if (Math.random() > 0.5) {
+    current += change;
+  } else {
+    current -= change;
+  }
+
+  document.getElementById("fakeOnline").innerText =
+    "🟢 " + current + " member online";
+}
+setInterval(updateOnline, 1000);
+
+// NAMA USA & GERMANY
+const names = [
+  "James","Michael","John","Robert","David",
+  "William","Richard","Joseph","Thomas","Charles",
+  "Lukas","Leon","Finn","Jonas","Tim",
+  "Maximilian","Paul","Felix","Elias","Noah"
+];
+
+// AKSI
+const actions = ["joined", "left", "is online", "just joined"];
+
+function showUser() {
+  const el = document.getElementById("liveUser");
+
+  const name = names[Math.floor(Math.random() * names.length)];
+  const action = actions[Math.floor(Math.random() * actions.length)];
+
+  el.innerText = "🟢 " + name + " " + action;
+  el.style.opacity = 1;
+
+  setTimeout(() => {
+    el.style.opacity = 0;
+  }, 2000);
+}
+setInterval(showUser, 3000);
+
+// BACKGROUND SLIDE
+const images = ["https://images2.imgbox.com/bd/24/zaHRTHiG_o.jpg", "https://images2.imgbox.com/5a/c5/sKWeOOOC_o.jpg"];
 let index = 0;
 
 const bg1 = document.getElementById("bg1");
 const bg2 = document.getElementById("bg2");
 const bg = document.getElementById("bg");
 
-// set awal
 bg1.src = images[0];
 bg2.src = images[1];
 
@@ -24,7 +66,7 @@ function slideBg() {
   setTimeout(() => {
     bg.style.transition = "none";
     bg.style.transform = "translateX(0)";
-    
+
     bg1.src = images[index];
     bg2.src = images[(index + 1) % images.length];
 
@@ -33,26 +75,24 @@ function slideBg() {
     }, 50);
   }, 3000);
 }
-
 setInterval(slideBg, 4000);
 
-// DATA LINK
+// LINK
 const data = [
   {
-    name: "WhatsApp",
+    name: "WhatsApp GROUP",
     icon: "https://img.icons8.com/color/48/whatsapp.png",
-    link: "https://tapthelink.cfd/Pfrqy",
+    link: "https://api.whatsapp.com/send?phone=628123456789",
     class: "wa"
   },
   {
-    name: "Telegram",
+    name: "Telegram GROUP",
     icon: "https://img.icons8.com/color/48/telegram-app.png",
-    link: "https://tapthelink.cfd/Pfrqy",
+    link: "https://t.me/username",
     class: "telegram"
   }
 ];
 
-// RENDER
 const container = document.getElementById("links");
 
 data.forEach(item => {
